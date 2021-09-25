1d266759e30273c51f7b0a388aa439f250033cd4
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-09-25 20:53:59 +0700

    Repeat character n times

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -4,15 +4,25 @@ import "testing"
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("Repeat 'a' character 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+	t.Run("Repeat 'c; character n times", func(t *testing.T) {
+		repeated := Repeat("c", 10)
+		expected := "cccccccccc"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
 }
