dd4b0fa7719db5ef2b03c27c0ddafbd0a0fc402b
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-09-25 20:17:47 +0700

    Refactor code

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -8,17 +8,25 @@ const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return prefix
+}
+
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
-	} else if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
 }
 
 func main() {
