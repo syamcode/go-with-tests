730f45601d7e504c484a18d012f9c7d3814a368f
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-10-10 06:10:19 +0700

    refactor test, name struct field

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -17,9 +17,9 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{10.0, 5.0}, 50.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{shape: Rectangle{Width: 10.0, Height: 5.0}, want: 50.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
